0f2c0507f5bc279690c35d4cf1d3db24133398a2
hoycw <hoycw@berkeley.edu>
2019-04-24 22:28:43 -0700

    SBJ00 updated to match stroop; SGE for CP24.IR68

--- SBJ00_cleaning_prep.m
+++ SBJ00_cleaning_prep.m
@@ -13,8 +13,10 @@ function SBJ00_cleaning_prep(SBJ,plot_psd)
 %   SBJ_raw_labels.mat- the raw labels for all channels, to inform channel selection
 %   raw_psds/- PSDs of channels as determined by plot_psd
 
-addpath('/home/knight/hoycw/PRJ_Error/scripts/utils/');
-addpath('/home/knight/hoycw/Apps/fieldtrip/');
+if exist('/home/knight/hoycw/','dir');root_dir='/home/knight/hoycw/';ft_dir=[root_dir 'Apps/fieldtrip/'];
+else root_dir='/Volumes/hoycw_clust/';ft_dir='/Users/colinhoy/Code/Apps/fieldtrip/';end
+addpath([root_dir 'PRJ_Error/scripts/utils/']);
+addpath(ft_dir);
 ft_defaults
 
 % plot_psd      = '1by1';         % 'all','1by1','none'
@@ -30,81 +32,88 @@ for f_ix = 1:length(notch_freqs)
 end
 
 %% Processing
-SBJ_vars_cmd = ['run /home/knight/hoycw/PRJ_Error/scripts/SBJ_vars/' SBJ '_vars.m'];
+SBJ_vars_cmd = ['run ' root_dir 'PRJ_Error/scripts/SBJ_vars/' SBJ '_vars.m'];
 eval(SBJ_vars_cmd);
-fprintf('============== Processing %s, %s ==============\n',SBJ,SBJ_vars.raw_file);
 
-%% Set up directories
-% Assume only one block for now...
-%         if length(SBJ_files{SBJ_ix}{2})>1
-%             block_prefix = strcat('_B',num2str(file_ix));
-psd_dir = strcat(SBJ_vars.dirs.import,'raw_psds/');
-if ~exist(psd_dir,'dir')
-    mkdir(psd_dir);
-end
-
-%% Load the data
-if strcmp(SBJ_vars.raw_file(end-2:end),'mat')
-    load(SBJ_vars.dirs.raw_filename);
-else
-    cfg = [];
-    cfg.dataset = SBJ_vars.dirs.raw_filename;
-    cfg.continuous = 'yes';
-    cfg.channel = 'all';
-    data = ft_preprocessing(cfg);
-end
-
-%% Check for problematic labels
-for ch_ix = 1:numel(data.label)
-    if ~isempty(find(data.label{ch_ix}=='/',1))   % These will mess up paths when saved
-        data.label{ch_ix}(find(data.label{ch_ix}=='/')) = '_';
+for b_ix = 1:numel(SBJ_vars.raw_file)
+    fprintf('============== Processing %s, %s ==============\n',SBJ,SBJ_vars.raw_file{b_ix});
+    if numel(SBJ_vars.raw_file)>1
+        block_suffix = strcat('_',SBJ_vars.block_name{b_ix});
+    else
+        block_suffix = SBJ_vars.block_name{b_ix};   % should just be ''
     end
+    %% Set up directories
+    psd_dir = strcat(SBJ_vars.dirs.import,'raw_psds/');
+    if ~exist(psd_dir,'dir')
+        mkdir(psd_dir);
+    end
+    
+    %% Load the data
+    if strcmp(SBJ_vars.raw_file{b_ix}(end-2:end),'mat')
+        load(SBJ_vars.dirs.raw_filename{b_ix});
+    else
+        cfg = [];
+        cfg.dataset = SBJ_vars.dirs.raw_filename{b_ix};
+        cfg.continuous = 'yes';
+        cfg.channel = 'all';
+        data = ft_preprocessing(cfg);
+    end
+    
+    %% Check for problematic labels
+    for ch_ix = 1:numel(data.label)
+        if ~isempty(find(data.label{ch_ix}=='/',1))   % These will mess up paths when saved
+            data.label{ch_ix}(data.label{ch_ix}=='/') = '_';
+        end
+    end
+    
+    %% Check noise profile
+    if strcmp(plot_psd,'all')
+        fprintf('============== Plotting PSDs %s, %s ==============\n',SBJ,SBJ_vars.raw_file{b_ix});
+        fn_plot_PSD_all_save(data.trial{1},data.label,data.fsample,...
+            strcat(psd_dir,SBJ,'_raw_psd',block_suffix,'_all.',psd_fig_type));
+    elseif strcmp(plot_psd,'1by1')
+        fprintf('============== Plotting PSDs %s, %s ==============\n',SBJ,SBJ_vars.raw_file{b_ix});
+        fn_plot_PSD_1by1_save(data.trial{1},data.label,data.fsample,...
+            strcat(psd_dir,SBJ,'_raw_psd',block_suffix),psd_fig_type);
+    end
+    
+    %% Resample data to speed things up
+    if (resamp_it) && (data.fsample > resample_freq)
+        fprintf('============== Resampling %s, %s ==============\n',SBJ,SBJ_vars.raw_file{b_ix});
+        cfg_resamp = [];
+        cfg_resamp.resamplefs = resample_freq;
+        cfg_resamp.detrend = 'yes';
+        data = ft_resampledata(cfg_resamp, data);
+    end
+    
+    %% Filter data for ease of viewing
+    if (filter_it)% && (data_resamp.cfg.dftfreq ~= notch_freqs)
+        fprintf('============== Bandstop filtering %s, %s ==============\n',SBJ,SBJ_vars.raw_file{b_ix});
+        cfg_bs = [];
+        cfg_bs.continuous = 'yes';
+        cfg_bs.bsfilter   = 'yes';
+        bs_freq_lim(bs_freq_lim(:,2) > data.fsample/2, :) = [];
+        cfg_bs.bsfreq     = bs_freq_lim;
+        cfg_bs.bsfiltord  = 2;
+        cfg_bs.demean     = 'yes';
+        data = ft_preprocessing(cfg_bs, data);
+    end
+    
+    %% Save data
+    % Save preclean data
+    out_filename = strcat(SBJ_vars.dirs.preproc,SBJ,'_preclean',block_suffix,'.mat');
+    fprintf('============== Saving %s, %s ==============\n',out_filename,SBJ_vars.raw_file{b_ix});
+    save(out_filename, '-v7.3', 'data');
+    
+    % Save data labels
+    raw_labels = data.label;
+    label_fname = strcat(SBJ_vars.dirs.import,SBJ,'_raw_labels',block_suffix,'.mat');
+    % if raw_labels exit, load, compare, save if different
+    if ~exist(label_fname)
+        save(label_fname,'raw_labels');
+    else
+        warning(['WARNING: raw_labels filename already exists: ',label_fname,'\n NOT saving a new file...']);
+    end
+    
 end
-
-%% Check noise profile
-if strcmp(plot_psd,'all')
-    fprintf('============== Plotting PSDs %s, %s ==============\n',SBJ,SBJ_vars.raw_file);
-    fn_plot_PSD_all_save(data.trial{1},data.label,data.fsample,...
-        strcat(psd_dir,SBJ,'_raw_psd',SBJ_vars.block_prefix,'_all.',psd_fig_type));
-elseif strcmp(plot_psd,'1by1')
-    fprintf('============== Plotting PSDs %s, %s ==============\n',SBJ,SBJ_vars.raw_file);
-    fn_plot_PSD_1by1_save(data.trial{1},data.label,data.fsample,...
-        strcat(psd_dir,SBJ,'_raw_psd',SBJ_vars.block_prefix),psd_fig_type);
-end
-
-%% Resample data to speed things up
-if (resamp_it) && (data.fsample ~= resample_freq)
-    cfg_resamp = [];
-    cfg_resamp.resamplefs = resample_freq;
-    cfg_resamp.detrend = 'yes';
-    data = ft_resampledata(cfg_resamp, data);
-end
-
-%% Filter data for ease of viewing
-fprintf('============== Bandstop filtering %s, %s ==============\n',SBJ,SBJ_vars.raw_file);
-if (filter_it)% && (data_resamp.cfg.dftfreq ~= notch_freqs)
-    cfg_bs = [];
-    cfg_bs.continuous = 'yes';
-    cfg_bs.bsfilter = 'yes';
-    cfg_bs.bsfreq = bs_freq_lim;
-    cfg_bs.demean = 'yes';
-    data = ft_preprocessing(cfg_bs, data);
-end
-
-%% Save data
-% Save preclean data
-out_filename = strcat(SBJ_vars.dirs.preproc,SBJ,'_preclean',SBJ_vars.block_prefix,'.mat');
-fprintf('============== Saving %s, %s ==============\n',out_filename,SBJ_vars.raw_file);
-save(out_filename, '-v7.3', 'data');
-
-% Save data labels
-raw_labels = data.label;
-label_filename = strcat(SBJ_vars.dirs.import,SBJ,'_raw_labels',SBJ_vars.block_prefix,'.mat');
-% if raw_labels exit, load, compare, save if different
-if ~exist(label_filename)
-    save(label_filename,'raw_labels');
-else
-    error(strcat('ERROR: raw_labels filename already exists: ',label_filename));
-end
-
 end
