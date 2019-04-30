900faaff44b2de225553412d9e817d45bba2f11d
hoycw <hoycw@berkeley.edu>
2019-04-30 10:26:17 -0700

    IR66 and IR74 initial SBJ_vars

--- SBJ_vars/IR66_vars.m
+++ SBJ_vars/IR66_vars.m
@@ -36,31 +36,47 @@ end
 
 SBJ_vars.dirs.raw_filename = strcat(SBJ_vars.dirs.raw,SBJ_vars.raw_file);
 
+SBJ_vars.recon.surf_l     = [SBJ_vars.dirs.recon 'Surfaces/' SBJ_vars.SBJ '_cortex_lh.mat'];
+SBJ_vars.recon.surf_r     = [SBJ_vars.dirs.recon 'Surfaces/' SBJ_vars.SBJ '_cortex_rh.mat'];
+SBJ_vars.recon.elec_pat   = [SBJ_vars.dirs.recon 'Electrodes/' SBJ_vars.SBJ '_elec_acpc_f.mat'];
+SBJ_vars.recon.elec_mni_v = [SBJ_vars.dirs.recon 'Electrodes/' SBJ_vars.SBJ '_elec_mni_frv.mat'];
+SBJ_vars.recon.elec_mni_s = [];
+SBJ_vars.recon.fs_T1      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_postop_T1.mgz'];
+SBJ_vars.recon.fs_DK      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_postop_aparc+aseg.mgz'];
+SBJ_vars.recon.fs_Dx      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_postop_aparc.a2009s+aseg.mgz'];
+
 %--------------------------------------
 % Channel Selection
 %--------------------------------------
-hdr = ft_read_header(SBJ_vars.dirs.raw_filename);
-SBJ_vars.orig_n_ch = length(hdr.label);
-SBJ_vars.orig_n_samples = hdr.nSamples;
-SBJ_vars.orig_srate = hdr.Fs;
-clear hdr;
+SBJ_vars.ch_lab.probes     = {'RAM','RHH','RTH','RAC','ROF','RPC','LAM','LHH','LTH','LAC','LOF','LPC'};
+SBJ_vars.ch_lab.probe_type = {'seeg','seeg','seeg','seeg','seeg','seeg','seeg','seeg','seeg','seeg','seeg','seeg'};
+SBJ_vars.ch_lab.ref_type   = {'BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP'};
+SBJ_vars.ch_lab.ROI        = {'all'};
+SBJ_vars.ch_lab.eeg_ROI    = {};
 
-SBJ_vars.ch_lab.probes = {};
-SBJ_vars.ch_lab.ref_type     = {};
-SBJ_vars.ch_lab.ROI    = {};
-SBJ_vars.ch_lab.eeg_ROI = {};
 
 %SBJ_vars.ch_lab.prefix = ''; %'POL ';    % before every channel except 'EDF Annotations'
 %SBJ_vars.ch_lab.suffix = ''; %'-Ref';    % after every channel except 'EDF Annotations'
-%SBJ_vars.ch_lab.mislabel = {};% {{'ch1bad','ch1good'}};
+SBJ_vars.ch_lab.mislabel = {{'CZ ','CZ'}};
 
 SBJ_vars.ch_lab.ref_exclude = {}; %exclude from the CAR
 SBJ_vars.ch_lab.bad = {...
+    'LHH1','LHH2','LHH3','LTH1','LTH2','LTH3','LTH4',...% epileptic
+    'RHH1','RHH2','RHH3','RHH4','RTH1','RTH2','RTH3',...% epileptic, originally tried then stopped saving RAM1, RAM2
+    'RAM1','RAM2','LAM1','LAM2',...% rejected after preproc cleaning
+    'LAC8','LAC9','LAC10',...% out of brain
+    'DC01','DC03','DC04',...% empty analogs
+    'GND','REF','EKG'...% not real data
     };
-SBJ_vars.ch_lab.eeg = {};
-% SBJ_vars.ch_lab.CZ_lap_ref = {};
-SBJ_vars.ch_lab.eog = {};
-SBJ_vars.ch_lab.photod = {};
+% EmoDim Original cleaning:
+%   NOTE: all except LAM1,2 and RAM1 were kept using channel specific epoch rejection,
+%           so the below were my original notes before that strategy.
+%     'LHH4','LHH10','LTH5','LTH6',...% tossed after looking at preproc
+%     'LAM1','LAM2','LAM3','LAM4','LAM5','LAM6',...% tossed for spread after looking at preproc
+%     'RAM1','RAM2',...% tossed after looking at preproc
+SBJ_vars.ch_lab.eeg = {'FZ','CZ ','OZ','C3','C4'};
+SBJ_vars.ch_lab.eog = {'LUE','LLE','RUE','RLE'};
+SBJ_vars.ch_lab.photod = {'DC02'};
 
 %--------------------------------------
 % Line Noise Parameters
@@ -71,17 +87,17 @@ SBJ_vars.bs_width    = 2;
 %--------------------------------------
 % Time Parameters
 %--------------------------------------
-SBJ_vars.analysis_time = {};
+SBJ_vars.analysis_time = {{[85.0 1431.09]}};
 SBJ_vars.ignore_trials = [];
 
 %--------------------------------------
 % Artifact Rejection Parameters
 %--------------------------------------
 % SBJ_vars.artifact_params.std_limit_raw = 7;
-% SBJ_vars.artifact_params.hard_threshold_raw = 1000;
+% SBJ_vars.artifact_params.hard_threshold_raw = 700;
 
 % SBJ_vars.artifact_params.std_limit_diff = 7;
-% SBJ_vars.artifact_params.hard_threshold_diff = 100;
+% SBJ_vars.artifact_params.hard_threshold_diff = 70;
 
 %--------------------------------------
 % Trials to Reject
--- SBJ_vars/IR74_vars.m
+++ SBJ_vars/IR74_vars.m
@@ -9,7 +9,7 @@ SBJ_vars.SBJ        = 'IR74';
 SBJ_vars.raw_file   = {'2008123123_0001.besa'};
 SBJ_vars.block_name = {''};
 SBJ_vars.low_srate  = [0];
-SBJ_vars.log_fname  = {''};
+SBJ_vars.log_fname  = {'ir742_response_log_20180327170333.txt'};
 
 SBJ_vars.dirs.SBJ     = [root_dir 'PRJ_Error/data/' SBJ_vars.SBJ '/'];
 SBJ_vars.dirs.raw     = [SBJ_vars.dirs.SBJ '00_raw/'];
@@ -36,19 +36,27 @@ end
 
 SBJ_vars.dirs.raw_filename = strcat(SBJ_vars.dirs.raw,SBJ_vars.raw_file);
 
+SBJ_vars.recon.surf_l     = [SBJ_vars.dirs.recon 'Surfaces/' SBJ_vars.SBJ '_cortex_lh.mat'];
+SBJ_vars.recon.surf_r     = [SBJ_vars.dirs.recon 'Surfaces/' SBJ_vars.SBJ '_cortex_rh.mat'];
+SBJ_vars.recon.elec_pat   = [SBJ_vars.dirs.recon 'Electrodes/' SBJ_vars.SBJ '_elec_acpc_f.mat'];
+SBJ_vars.recon.elec_mni_v = [SBJ_vars.dirs.recon 'Electrodes/' SBJ_vars.SBJ '_elec_mni_frv.mat'];
+SBJ_vars.recon.elec_mni_s = [];
+SBJ_vars.recon.fs_T1      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_preop_T1.mgz'];
+SBJ_vars.recon.fs_DK      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_preop_aparc+aseg.mgz'];
+SBJ_vars.recon.fs_Dx      = [SBJ_vars.dirs.recon 'Scans/' SBJ_vars.SBJ '_fs_preop_aparc.a2009s+aseg.mgz'];
+
 %--------------------------------------
 % Channel Selection
 %--------------------------------------
-hdr = ft_read_header(SBJ_vars.dirs.raw_filename);
-SBJ_vars.orig_n_ch = length(hdr.label);
-SBJ_vars.orig_n_samples = hdr.nSamples;
-SBJ_vars.orig_srate = hdr.Fs;
-clear hdr;
-
-SBJ_vars.ch_lab.probes = {};
-SBJ_vars.ch_lab.ref_type     = {};
-SBJ_vars.ch_lab.ROI    = {};
-SBJ_vars.ch_lab.eeg_ROI = {};
+SBJ_vars.ch_lab.probes     = {'LAM','LHH','LTH','LIN','LOF','LAC','LPRE',...
+                              'RAM','RHH','RTH','ROF','RIN','RAC','RPRE'};
+SBJ_vars.ch_lab.probe_type = {'seeg','seeg','seeg','seeg','seeg','seeg','seeg',...
+                              'seeg','seeg','seeg','seeg','seeg','seeg','seeg'};
+SBJ_vars.ch_lab.ref_type   = {'BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP','BP'};
+if ~all(numel(SBJ_vars.ch_lab.probes)==[numel(SBJ_vars.ch_lab.probe_type) numel(SBJ_vars.ch_lab.ref_type)]);
+    error('probes ~= type+ref');end;
+SBJ_vars.ch_lab.ROI        = {'all'};%'LAC*','LIN*','LOF*','LPRE*','RAC*','RIN*','ROF*','RPRE*'};
+SBJ_vars.ch_lab.eeg_ROI    = {'CZ'};
 
 %SBJ_vars.ch_lab.prefix = ''; %'POL ';    % before every channel except 'EDF Annotations'
 %SBJ_vars.ch_lab.suffix = ''; %'-Ref';    % after every channel except 'EDF Annotations'
@@ -56,11 +64,21 @@ SBJ_vars.ch_lab.eeg_ROI = {};
 
 SBJ_vars.ch_lab.ref_exclude = {}; %exclude from the CAR
 SBJ_vars.ch_lab.bad = {...
+    'LTH1','LTH2','LHH1','LHH2','LHH3','LHH4','LAM1','LAM2','LAM3','LHH5',...% epileptic
+    'RTH2','RTH3','RHH2','RHH3','RHH4','RHH5','RAM1','RAM2','RAM3',...% epileptic
+    'LTH3','LHH6','LAM4','RAM4','RHH1','RTH1',...% spread (addeed 3/2/19 viewing)
+    'RTH7','RTH8',...% spiking artifact
+    'RTH10','RIN9','RIN10','RPRE10',...% out of brain (based on my eye, maybe add RAC10 and LIN10 since they're edge)
+    'DC03','DC04','G','EKG','REF','E'...% not real data
     };
-SBJ_vars.ch_lab.eeg = {};
+% bad_codes: 1 = toss (epileptic or bad); 2 = suspicious; 3 = out of brain; 0 = junk
+SBJ_vars.ch_lab.bad_type = {'bad','sus','out'};
+SBJ_vars.ch_lab.bad_code = [1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 2 2 2 2 2 2 2 2 3 3 3 3 0 0 0 0 0 0];
+if numel(SBJ_vars.ch_lab.bad)~=numel(SBJ_vars.ch_lab.bad_code);error('bad ~= bad_code');end
+SBJ_vars.ch_lab.eeg = {'FPZ','CZ','OZ','C3','C4'};
 % SBJ_vars.ch_lab.CZ_lap_ref = {};
-SBJ_vars.ch_lab.eog = {};
-SBJ_vars.ch_lab.photod = {};
+SBJ_vars.ch_lab.eog = {'LUC','LLC','RUC','RLC'};
+SBJ_vars.ch_lab.photod = {'DC01'};
 
 %--------------------------------------
 % Line Noise Parameters
@@ -78,7 +96,7 @@ SBJ_vars.ignore_trials = [];
 % Artifact Rejection Parameters
 %--------------------------------------
 % SBJ_vars.artifact_params.std_limit_raw = 7;
-% SBJ_vars.artifact_params.hard_threshold_raw = 1000;
+% SBJ_vars.artifact_params.hard_threshold_raw = 800;
 
 % SBJ_vars.artifact_params.std_limit_diff = 7;
 % SBJ_vars.artifact_params.hard_threshold_diff = 100;
