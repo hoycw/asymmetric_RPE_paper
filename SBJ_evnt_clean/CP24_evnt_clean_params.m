686a481435ab43dc573fe7816a29a5d991a70a8d
hoycw <hoycw@berkeley.edu>
2019-04-30 15:14:02 -0700

    CP24 evnt clean for single run

---
+++ SBJ_evnt_clean/CP24_evnt_clean_params.m
@@ -0,0 +1,34 @@
+%% Photodiode Trace Cleaning Parameters: CP24 
+% Mark trials to ignore e.g., interruptions
+ignore_trials = [111];
+
+% Fix drift over first half of recording
+x = [1:find(evnt.time{1}==867)];
+y = -0.000042201*x + 283.93;
+evnt.trial{1}(x) = evnt.trial{1}(x)-y;
+% Remove offset in remainder
+evnt.trial{1}(x(end)+1:end) = evnt.trial{1}(x(end)+1:end)-249;
+
+% Set zero/baseline during a block
+bsln_val = 0;
+
+% Record epochs (in sec) with fluctuations that should be set to baseline
+bsln_times = {...
+    [0.0 157.0],...%restart
+    [613.0 615.9]...% B2T1 (trial 111, patient wasn't paying attention)
+    };
+% Record epochs (in sec) when baseline has shifted
+bsln_shift_times = {};
+% Record amount of shift in baseline for each epoch 
+bsln_shift_val = [];
+if length(bsln_shift_times)~=length(bsln_shift_val)
+    error('Number of epochs and values for baseline shift periods do not match.');
+end
+
+% Record within trial corrections
+stim_times = {};
+stim_yval = [];
+if length(stim_times)~=length(stim_yval)
+    error('Number of epochs and values for stimulus correction periods do not match.');
+end
+
