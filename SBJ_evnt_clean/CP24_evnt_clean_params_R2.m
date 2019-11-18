7d66529deb1b7a9c4aeb2339e901941d459e1dce
hoycw <hoycw@berkeley.edu>
2019-11-18 12:19:07 -0800

    update to handle multi-run concat

--- SBJ_evnt_clean/CP24_evnt_clean_params_R2.m
+++ SBJ_evnt_clean/CP24_evnt_clean_params_R2.m
@@ -2,19 +2,18 @@
 % Mark trials to ignore e.g., interruptions
 ignore_trials = [];
 
-% Fix drift over first half of recording
-x = [1:find(evnt.time{1}==867)];
-y = -0.000042201*x + 283.93;
-evnt.trial{1}(x) = evnt.trial{1}(x)-y;
-% Remove offset in remainder
-evnt.trial{1}(x(end)+1:end) = evnt.trial{1}(x(end)+1:end)-249;
+%  % Fix drift over first half of recording
+%  x = [1:find(evnt.time{1}==867)];
+%  y = -0.000042201*x + 283.93;
+%  evnt.trial{1}(x) = evnt.trial{1}(x)-y;
+%  % Remove offset in remainder
+%  evnt.trial{1}(x(end)+1:end) = evnt.trial{1}(x(end)+1:end)-249;
 
 % Set zero/baseline during a block
-bsln_val = 0;
+bsln_val = 116.2;
 
 % Record epochs (in sec) with fluctuations that should be set to baseline
 bsln_times = {...
-    [0.0 157.0]...%restart
     };
 % Record epochs (in sec) when baseline has shifted
 bsln_shift_times = {};
